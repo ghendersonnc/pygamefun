59fa3bde176748e6f78172c74fb9e15e1cdf2fb5
ghendersonnc <ghendersonnc@gmail.com>
2023-02-13 23:19:24 -0500

    Enemy class

--- main.py
+++ main.py
@@ -3,52 +3,7 @@ import time
 import random
 
 from player import Player
-
-
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self, enemy_type):
-        super().__init__()
-        self.type = enemy_type
-        if enemy_type == 'fly':
-            fly_1 = pygame.image.load('graphics/Fly/Fly1.png')
-            fly_2 = pygame.image.load('graphics/Fly/Fly2.png')
-            self.frames = [fly_1, fly_2]
-            position_y = sky_surface_height - 100
-        else:
-            snail_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-            snail_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
-            self.frames = [snail_1, snail_2]
-            position_y = sky_surface_height
-
-        self.animation_index = 0
-        self.image = self.frames[self.animation_index]
-        self.rect = self.image.get_rect(bottomleft=(random.randint(900, 1100), position_y))
-        self.position_x = self.rect.x
-        self.position_y = self.rect.y
-
-    def animation_state(self, delta_times):
-        if self.type == 'fly':
-            self.animation_index += delta_times * 5
-        else:
-            self.animation_index += delta_times * 2
-
-        if self.animation_index >= len(self.frames):
-            self.animation_index = 0
-        self.image = self.frames[int(self.animation_index)]
-
-    def move(self, delta_times):
-        self.position_x -= 400 * delta_times
-        self.rect.x = self.position_x
-
-    def destroy(self):
-        if self.rect.x < -100:
-            self.kill()
-
-    def update(self, **kwargs):
-        self.animation_state(kwargs['delta_time'])
-        self.move(kwargs['delta_time'])
-        self.destroy()
-
+from enemy import Enemy
 
 def display_score():
     current_score = int(pygame.time.get_ticks() / 1000) - start_time
@@ -120,7 +75,7 @@ while running:
 
         else:
             if event.type == enemy_timer:
-                enemy_group.add(Enemy(random.choice(['fly', 'snail'])))
+                enemy_group.add(Enemy(random.choice(['fly', 'snail']), sky_surface_height))
 
     if not game_over:
         SCREEN.blit(sky_surface, (0, 0))
